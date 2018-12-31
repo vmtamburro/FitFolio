febcc115ccc69c3260162406508eb60ee1934e7b
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2018-12-31 16:44:57 -0500

    Add files via upload
    Created UI for Main Screen.

---
+++ MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;C:\Users\Tori's PC\Desktop\FitFolio\FitFolioDemo\MainActivity.cs
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace FitFolio
+{
+    [Activity(Label = "FitFolio", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
